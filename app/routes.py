9e98cbfba754c83abe60d5e9ff2e25536f43a95c
James Grantham <granthamjames@yahoo.com>
2023-02-01 21:01:40 -0700

    connected sqlite3 to project

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,23 @@
-test
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    me = {
+        "first_name": "James",
+        "last_name": "Grantham",
+        "hobbies": "Skateboarding",
+        "is_active": True
+    }
+
+    return me
+
+
+
+
+
+
+
+
+#homework object oriented programming
